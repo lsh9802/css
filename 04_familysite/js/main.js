b5850b91fd66dcc324f3378a02abf59ad0c00651
lsh9802 <guatelee@naver.com>
2025-06-17 17:29:22 +0900

    Update main.js

--- 04_familysite/js/main.js
+++ 04_familysite/js/main.js
@@ -1,10 +1,13 @@
 $(function(){
     $(".btn1").click(function(){
         $(".list1").toggle();
+        $(".list2").hide();
+
     })
 
     $(".btn2").click(function(){
         $(".list2").toggle();
+        $(".list1").hide();
     })
 
 
