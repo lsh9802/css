7790c81bd155a23c0b5fe0c6f306d98ab6af0c7c
lsh9802 <guatelee@naver.com>
2025-06-18 17:31:53 +0900

    Add files via upload

---
+++ 05_slide(coffee)/js/main.js
@@ -0,0 +1,17 @@
+$(function(){
+    // slick 슬라이드 자동 실행
+    $('.slider').slick({
+    autoplay: true,
+    // fadeInFadeout 슬라이드
+    fade:true,
+    // 페이지버튼 보이게하기
+    dots:true
+    });
+
+
+
+
+
+
+    
+})
